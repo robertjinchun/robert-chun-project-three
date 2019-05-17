2df1f95b98260a470b9c06cbb3ba6fa345515202
robertjinchun <robertjinchun@outlook.com>
2019-05-16 20:10:40 -0400

    Main functionality of javascript is completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,78 @@
 //Javascript portion of my type game
 const legendsType = {};
-let arrayOfIndividualCharacters = [];
+// legendsType.arrayOfIndividualCharacters = {};
 let wordGen;
+let globalTimer;
 
 legendsType.easyMode = ['yuck', 'zeal', 'zoic', 'abys', 'aced', 'aced', 'acro', 'ditt', 'door', 'flex', 'wave'];
+legendsType.letterCounter = 0;
+legendsType.wordCounter = 0;
+legendsType.currentWord = [];
 
 //I will need a function for a random number generator to display the total amount of words in my game
 legendsType.randomNumberGen = function () {
     return Math.floor(Math.random() * 11);
 }
 
-legendsType.start = function(){
-    $('.start-game').on('click',function(event){
-    // e.preventDefault();
-    wordGen = legendsType.randomNumberGen();
-    console.log (wordGen)
-    legendsType.currentIndividualLetterSplitter(legendsType.easyMode[wordGen]);
-
+legendsType.start = function () {
+    $('.start-game').on('click', function (event) {
+        // console.log($.now());
+        // e.preventDefault();
+        wordGen = legendsType.randomNumberGen();
+        // console.log(wordGen)
+        legendsType.currentIndividualLetterSplitter(legendsType.easyMode[wordGen]);
+        // return
     })
+}
+
+legendsType.midGame = function () {
+
 }
 
 //I need a function to split each word of my array in to an array of chacters
 
 legendsType.currentIndividualLetterSplitter = function (currentWord) {
+    let arrayOfIndividualCharacters = [];
     arrayOfIndividualCharacters = [...currentWord];
+    //displays the split letters in the boxes
     $('.first-letter').html(`<h2 class="">${arrayOfIndividualCharacters[0]}</h2>`);
     $('.second-letter').html(`<h2 class="">${arrayOfIndividualCharacters[1]}</h2>`);
     $('.third-letter').html(`<h2 class="">${arrayOfIndividualCharacters[2]}</h2>`);
     $('.fourth-letter').html(`<h2 class="">${arrayOfIndividualCharacters[3]}</h2>`);
+    legendsType.currentWord = arrayOfIndividualCharacters;
+    console.log(legendsType.currentWord);
+    return arrayOfIndividualCharacters;
 }
 
 
 //I need a function for event handling my individual keys on the keyboard
 
 legendsType.keyPressed = function () {
+    let currentLetter;
+    let currentNumber = [];
     // This event handels all the key presses
     $(window).keypress(function (event) {
-        //Obtains which key was pressed in the keyboard
-        let keyLetter = event.which;
-        console.log(event.which);
-        $('.user-typed-letter').html(`<h2 class="">${keyLetter}</h2>`);
         
+        currentNumber = event.which;
+        // console.log(currentNumber[0]);
+        currentLetter = String.fromCharCode(event.which);
+        console.log(currentLetter);
+        $('.user-typed-letter').html(`<h2 class="">${currentLetter}</h2>`);
+        console.log(legendsType.currentWord);
         
-
+        if (currentLetter === legendsType.currentWord[legendsType.letterCounter]){
+            console.log(`im working`);
+            legendsType.letterCounter++;
+            console.log(legendsType.letterCounter);
+            if (legendsType.letterCounter === 4){
+                legendsType.letterCounter =0
+                wordGen = legendsType.randomNumberGen();
+                legendsType.currentIndividualLetterSplitter(legendsType.easyMode[wordGen]);
+            }
+        }
     })
+    // console.log($keyLetter);
+    //return $keyLetter;
 }
 
 
@@ -55,9 +84,10 @@ legendsType.keyPressed = function () {
 //if all the letters of the word have been matched, generate another random number and 
 
 $(function () {
-
+    //init
     legendsType.start();
     // legendsType.currentIndividualLetterSplitter(legendsType.easyMode[wordGen]);
     legendsType.keyPressed();
-
+    // console.log(console.log(legendsType.keyPressed ()));
+    //console.log(legendsType.keyPressed ());
 });
